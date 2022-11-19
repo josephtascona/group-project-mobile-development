b3d3b0db0528e530c57edd9e475c3576d7299874
Joseph Tascona <joseph.tascona@gmail.com>
2022-11-19 17:34:04 -0500

    Initial Commit

---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Joseph Tascona on 2022-11-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
