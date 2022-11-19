d9e87619acd3f05a5347a9f8bed79f49e169daea
Joseph Tascona <joseph.tascona@gmail.com>
2022-11-19 18:08:34 -0500

    Added logic

--- ExpenseTracker/ViewController.swift
+++ ExpenseTracker/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var expenseField: UITextField!
+    
+    @IBOutlet weak var costField: UITextField!
+    
+    @IBOutlet weak var textbox: UITextView!
+    
+    var expenses: [String] = []
+    var costs: [Double] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func addBtn(_ sender: Any) {
+        expenses.append(expenseField.text!)
+        costs.append(Double(costField.text!)!)
+        textbox.text = ""
+        for i in 0...(costs.count-1) {
+            var formatCost = String(format: "%.2f", costs[i])
+            textbox.text += "Expense: " + expenses[i] + " Cost: " + formatCost + "$\n"
+        }
+        expenseField.text = ""
+        costField.text = ""
+    }
 
 }
 
